5c501abc749e74b35b672d5dd93e59bf71df412a
harjeevansingh <harjeevansingh01@gmail.com>
2022-04-23 18:08:16 +0530

    chore: first commit

---
+++ src/main/java/com/worldGo/WorldGoApplication.java
@@ -0,0 +1,13 @@
+package com.worldGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldGo/WorldGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
